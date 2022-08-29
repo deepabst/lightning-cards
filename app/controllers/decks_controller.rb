9237b4ef9b59a8562094204f05d7fe523621b732
Dee Pabst <dee@canva.com>
2022-08-29 18:20:12 +1000

    adding in CRUD for decks

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -2,10 +2,29 @@ class DecksController < ApplicationController
 
   before_action :check_if_logged_in
   
+  def new
+    @deck = Deck.new
+  end # new
+  
   def create
-  end
+
+    # create a new deck with only a name
+    @deck = Deck.new deck_params
+    # associate the current user to the deck
+    @deck.user_id = @current_user.id
+    # save deck in the db
+    @deck.save
+
+    if @deck.persisted?
+      redirect_to decks_path
+    else
+      render :new
+    end # if @deck persisted
+    
+  end # create
 
   def index
+    @decks = Deck.where(user_id: @current_user.id)
   end
 
   def show
@@ -19,4 +38,13 @@ class DecksController < ApplicationController
 
   def destroy
   end
-end
+
+
+  private
+
+  def deck_params
+    params.require(:deck).permit(:name)
+  end
+  
+  
+end # class DecksController
