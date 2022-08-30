123c4a72b8a4faf8b3d03f3d32906c042e7580f9
Dee Pabst <dee@canva.com>
2022-08-30 12:10:00 +1000

    added edit and update Deck to controller moved deck form to a partial created Card controllers

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -7,7 +7,6 @@ class DecksController < ApplicationController
   end # new
   
   def create
-
     # create a new deck with only a name
     @deck = Deck.new deck_params
     # associate the current user to the deck
@@ -28,23 +27,52 @@ class DecksController < ApplicationController
   end
 
   def show
-  end
+    @deck = Deck.find params[:id]
+    # check the user owns this deck (might have guessed the url)
+    if @deck.user_id != @current_user.id
+      flash[:error] = "Deck not found" 
+      redirect_to decks_path
+    end
+  end # show
 
   def edit
+    # edit a deck name
+    @deck = Deck.find params[:id]
+    if @deck.user_id != @current_user.id
+      flash[:error] = "Deck not found" 
+      redirect_to decks_path
+    end
   end
-
+  
   def update
+    deck = Deck.find params[:id]
+    if deck.user_id != @current_user.id
+      flash[:error] = "Deck not found" 
+      redirect_to decks_path
+    else
+      deck.update deck_params
+      redirect_to deck_path(deck.id)
+    end
   end
 
   def destroy
+    # check if this deck belongs to the current user, if true delete it
+    @deck = Deck.find params[:id]
+    # check the user owns this deck (might have guessed the url)
+    if @deck.user_id != @current_user.id
+      flash[:error] = "Deck not found" 
+    else
+      Deck.destroy params[:id]
+    end
+    redirect_to decks_path
   end
-
 
   private
 
   def deck_params
     params.require(:deck).permit(:name)
   end
-  
+
+  # TODO: move check deck ownership code to a separate private method and run before anything else
   
 end # class DecksController
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   post '/login' => 'session#create'     # form submit, perform login, redirect
   delete '/login' => 'session#destroy'  # logout link, perform logour, redirect
 
-resources :decks, :users
+resources :decks, :users, :cards
 
 end #routes
