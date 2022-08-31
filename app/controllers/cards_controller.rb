197ef4261851f1f0567331fbdeabc5c907215983
Dee Pabst <dee@canva.com>
2022-08-31 13:02:05 +1000

    added more CRUD for Cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,21 +6,37 @@ class CardsController < ApplicationController
   def create
     @card = Card.new card_params
     # TODO: check if the deck id provided belongs to the current user (we got it from a form)
-  end
+    @card.save # creates new card in db
+
+    if @card.persisted?
+      redirect_to deck_path(@card.deck_id)
+    else
+      render :new
+    end # if
+  end # create
 
   def index
   end
 
   def show
+    @card = Card.find params[:id]
+    if @card.deck.user_id != @current_user.id
+      redirect_to root_path
+    end
   end
 
   def edit
+    @card = Card.find params[:id]
   end
 
   def update
+    card = Card.find params[:id]
+    card.update card_params
+    redirect_to card_path(card.id)
   end
 
   def destroy
+    Card.destroy params[:id]
   end
 
   private
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,5 @@
 class Card < ApplicationRecord
-    belongs_to :deck, optional: true
+  belongs_to :deck, optional: true
+
+  validates :deck_id, presence: true
 end
